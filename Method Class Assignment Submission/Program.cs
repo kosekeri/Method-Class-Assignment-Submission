df065ee3527766f8c369c05593aa366b9cb6921f
koseke <koseke@RUK1-5CG14864VQ>
2024-12-11 10:18:30 +0000

    Add project files.

---
+++ Method Class Assignment Submission/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace ConsoleAppExample
+{
+    // This is the class where the void method is defined
+    class MathOperations
+    {
+        // This method takes two integers as parameters
+        // Performs a math operation on the first integer (e.g., multiply by 2)
+        // Displays the second integer
+        public void PerformOperation(int num1, int num2)
+        {
+            // Perform a math operation on num1 and display the result
+            int result = num1 * 2; // Multiply num1 by 2
+            Console.WriteLine($"Result of operation on first number: {result}");
+
+            // Display the second integer
+            Console.WriteLine($"Second number provided: {num2}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call the PerformOperation method, passing in two numbers
+            mathOps.PerformOperation(5, 10);
+
+            // Call the PerformOperation method again, specifying the parameters by name
+            mathOps.PerformOperation(num1: 8, num2: 20);
+
+            // Pause the console to allow the user to see the output
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Method Class Assignment Submission/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Method Class Assignment Submission")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Amazon.com")]
+[assembly: AssemblyProduct("Method Class Assignment Submission")]
+[assembly: AssemblyCopyright("Copyright © Amazon.com 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5108da6-eb48-4457-b1af-c8531f21a9f2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
